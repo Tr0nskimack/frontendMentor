c4e086068295e4f8886c2682760b19aa8acabdb3
unknown <jmarcanoy@gmail.com>
2024-02-18 18:14:22 -0400

    modificacion nombres

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
           <p className="text-xs font-semibold text-[#c5f82a]">London, United Kingdom</p>
           <p className="mt-5 font-semibold text-xs text-[#b3b3b3]">"Front-end developer and avid reader."</p>
           <button className="text-xs hover:text-black  font-semibold mt-5 bg-[#333333] py-3 w-[270px] rounded-md shadow-sm hover:shadow-md hover:bg-[#c5f82a] transition-all">GitHub</button>
-          <button className="text-xs text-gray-100 font-semibold mt-4 bg-[#333333] py-3 w-[270px] rounded-md shadow-sm hover:shadow-md hover:bg-[#c5f82a] hover:text-black transition-all">Fronrend Mentor</button>
+          <button className="text-xs text-gray-100 font-semibold mt-4 bg-[#333333] py-3 w-[270px] rounded-md shadow-sm hover:shadow-md hover:bg-[#c5f82a] hover:text-black transition-all">Frontend Mentor</button>
           <button className="text-xs text-gray-100 font-semibold mt-4 bg-[#333333] py-3 w-[270px] rounded-md shadow-sm hover:shadow-md hover:bg-[#c5f82a] hover:text-black transition-all">Linkedin</button>
           <button className="text-xs text-gray-100 font-semibold mt-4 bg-[#333333] py-3 w-[270px] rounded-md shadow-sm hover:shadow-md hover:bg-[#c5f82a] hover:text-black transition-all">Twitter</button>
           <button className="text-xs text-gray-100 font-semibold mt-4 bg-[#333333] py-3 w-[270px] rounded-md shadow-sm hover:shadow-md hover:bg-[#c5f82a] hover:text-black transition-all">Instagram</button>
